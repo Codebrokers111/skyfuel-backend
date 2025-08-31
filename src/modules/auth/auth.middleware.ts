74f2520ceb57f4d1d4b911ec252feb784bbe9d5c
Mohd Zain <zainmohd1998@gmail.com>
2025-08-31 23:04:15 +0530

    add middleware to verify token for gettng user details

---
+++ src/modules/auth/auth.middleware.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from 'express';
+import jwt from "jsonwebtoken";
+
+// ========== AUTH MIDDLEWARE =================
+// checks JWT is valid when fetching user details or user is updating their profile
+// ============================================
+
+export interface AuthRequest extends Request {
+  userId?: string;
+}
+
+export function authenticate(req: AuthRequest, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Missing or invalid token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET!) as { sub: string };
+    req.userId = payload.sub;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid or expired token" });
+  }
+}
